04fcfca64a0254c92c7179fb1c341af8a3c21436
JasonOP69 <JasonOP69@users.noreply.github.com>
2026-07-06 18:04:55 +0300

    Upload BloxGifts site

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 const port = 5000;
 
-app.use(express.static('blox.gifts'));
+app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'blox.gifts', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, '0.0.0.0', () => {
